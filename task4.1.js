154c92fc95496eda28013723c6cfcbff9ab408a1
mikk369 <mikk.ereline@vikk.ee>
2021-09-15 17:54:07 +0300

    colors2

---
+++ task4.1.js
@@ -0,0 +1,22 @@
+const colors = ["Red", "Green", "White", "Black"];
+
+const a = colors.toString(); 
+console.log(a)
+//Output: Red,Green,White,Black
+
+const b = colors.join(", ");
+console.log(b)
+//Output: Red * Green * White * Black//
+
+const c = colors.join(" + ");
+console.log(c)
+//Output [ 'Red','Green','White' ]//
+
+
+
+
+
+
+
+
+  
