80b2822549c1a7da99d0da931f14279f818bab73
mikk369 <mikk.ereline@vikk.ee>
2021-09-15 17:49:30 +0300

    colors1

--- task4.js
+++ task4.js
@@ -1,4 +1,24 @@
-myColor = ["Red", "Green", "White", "Black"];
-console.log(myColor.toString());
-console.log(myColor.join());
-console.log(myColor.join('+'));
+const colors = ["Red", "Green", "White", "Black"];
+
+const a = colors.toString(); 
+console.log(a)
+//Output: Red,Green,White,Black
+
+const b = colors.join(" * ");
+console.log(b)
+//Output: Red * Green * White * Black//
+
+const c = colors.pop();
+console.log(colors)
+//Output [ 'Red','Green','White' ]//
+
+const d = colors.push('blue');
+console.log(colors)
+//output: [ 'Red','Green','White','Blue' ]//
+
+
+
+
+
+
+  
