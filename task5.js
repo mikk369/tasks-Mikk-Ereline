7465ccd0d848fcb5f841418b1867f5e923f18c8b
mikk369 <mikk.ereline@vikk.ee>
2021-09-15 19:44:27 +0300

    asd3

---
+++ task5.js
@@ -0,0 +1,12 @@
+function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+  console.log("")
+  console.log("tere")
+  console.log("minu nimi on john")
+
+  console.log("Hello World!" === "") //false
+console.log("" === "") //true
+  
+  console.log(capitalizeFirstLetter('tere')); //uppercase
+  console.log(capitalizeFirstLetter('minu nimi on john'));//uppercase
---
+++ task6.js
@@ -0,0 +1,7 @@
+function repeatStr(input, times) {
+    let result = input.repeat(times);
+    console.log(result);
+}
+repeatStr("Hello!", 2);
+repeatStr("Hello!" , 3)
+repeatStr("Hello!" , 12)
