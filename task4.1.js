901d462ad2c370918a4b33681dbdd04336a71144
mikk369 <mikk.ereline@vikk.ee>
2021-09-20 14:46:02 +0300

    colors2

--- task4.1.js
+++ task4.1.js
@@ -1,24 +1,17 @@
-const mycolors1 = ['Red'+','+'Green'+','+'White'+','+'Black' ] //prints colors without spaces//
+const mycolors = ["Red", "Green", "White", "Black"];
 
-for(let i = 0; i < mycolors1.length; i++){
-    console.log(mycolors1[i])    
-}
 
-const mycolors2 = ['Red'+', '+'Green'+', '+'White'+', '+'Black' ] //prints colors with spaces//
-
-for(let i = 0; i < mycolors2.length; i++){
-    console.log(mycolors2[i])    
-}
-
-const mycolors3 = ['Red'+' + '+'Green'+' + '+'White'+' + '+'Black'] // prints colors woth + and spaces//
-
-for(let i = 0; i < mycolors3.length; i++){
-    console.log(mycolors3[i])    
-}
+const a = mycolors.toString(); 
+console.log(a)
+//Output: Red,Green,White,Black
 
 
 
+const c = mycolors.join(", ");
+console.log(c)
+//Output [ 'Red', 'Green', 'White', 'Black' ]//
 
+const b = mycolors.join(" + ");
+console.log(b)
+//Output: Red + Green + White + Black//
 
-
-  
