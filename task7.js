b0e0e541423c3b906210ddf37b4993506b821638
mikk369 <mikk.ereline@vikk.ee>
2021-09-27 17:48:22 +0300

    genereerib suvalise numbri

--- task7.js
+++ task7.js
@@ -10,7 +10,7 @@ const LowestNumber = [39, 69, 96, 93, 63];
 console.log(Math.min(...LowestNumber))
 
 const EvenNumbers = [63, 6, 96, 93, 3];
-const filtered = EvenNumbers.filter(function (array) {  //Viimast punkti mina tööle ei saanud .//
+const filtered = EvenNumbers.filter(function (array) {  //Viimast punkti mina tööle ei saanud .
     return (array % 2 === 0);
 });
 console.log(filtered)
