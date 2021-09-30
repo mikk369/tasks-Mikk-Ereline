03bfbd6d586c6a3e09d49c12899826aeb0ded878
mikk369 <mikk.ereline@vikk.ee>
2021-09-30 16:08:40 +0300

    colors2

--- task5.js
+++ task5.js
@@ -2,7 +2,7 @@ function capitalizeFirstLetter(string) {
   return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
-const a1 = ' " '
+const a1 = ''
 const b2 = 'tere'
 const c3 = 'minu nimi on john'
 
@@ -10,8 +10,8 @@ console.log(a1)
 console.log(b2)
 console.log(c3)
 
-console.log(a1 , b2  === "") //false//
-console.log(c3 , "" === "") //true//
+console.log(a1, b2 === "") //false//
+console.log(c3, "" === "") //true//
 
 console.log(capitalizeFirstLetter(b2)); //uppercase
 console.log(capitalizeFirstLetter(c3)); //uppercase
