af4c071f6ad8320ec52c6c07eda6b054adc480a9
mikk369 <mikk.ereline@vikk.ee>
2021-09-30 13:06:27 +0300

    Colors

--- task4.js
+++ task4.js
@@ -1,26 +1,17 @@
-const mycolors1 = ['Red'+','+'Green'+','+'White'+','+'Black. ' ]
+const mycolors1 = ['Red' + ',' + 'Green' + ',' + 'White' + ',' + 'Black. ']
 
-for(let i = 0; i < mycolors1.length; i++){
-    console.log(mycolors1[i])    
+for (let i = 0; i < mycolors1.length; i++) {
+    console.log(mycolors1[i])
 }
 
-const mycolors2 = ['Red'+','+'Green'+','+'White'+','+'Black, ' ]
+const mycolors2 = ['Red' + ',' + 'Green' + ',' + 'White' + ',' + 'Black, ']
 
-for(let i = 0; i < mycolors2.length; i++){
-    console.log(mycolors2[i])    
+for (let i = 0; i < mycolors2.length; i++) {
+    console.log(mycolors2[i])
 }
 
-const mycolors3 = ['Red'+'+'+'Green'+'+'+'White'+'+'+'Black+ ']
+const mycolors3 = ['Red' + '+' + 'Green' + '+' + 'White' + '+' + 'Black+ ']
 
-for(let i = 0; i < mycolors3.length; i++){
-    console.log(mycolors3[i])    
+for (let i = 0; i < mycolors3.length; i++) {
+    console.log(mycolors3[i])
 }
-
-
-
-
-
-
-
-
-  
