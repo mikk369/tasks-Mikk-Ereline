b124f59b5d29c58764419f3a77d4399cb56a7d57
mikk369 <mikk.ereline@vikk.ee>
2021-09-30 12:57:23 +0300

    parandatud sulud

--- task2.js
+++ task2.js
@@ -1,13 +1,11 @@
-{
 const eesnimi = 'Mikk';
 const sunnipaev = 17;
 const sunnikuu = 11;
 const sunniaasta = 1989;
-const hobid = ['ujumine','videomängud'];
-const object = {eesnimi,sunnipaev,sunnikuu,sunniaasta,hobid}
+const hobid = ['ujumine', 'videomängud'];
+const object = { eesnimi, sunnipaev, sunnikuu, sunniaasta, hobid }
 
-console.log (object)
-console.log(sunnipaev+"."+sunnikuu+"."+sunniaasta)
-}
+console.log(object)
+console.log(sunnipaev + "." + sunnikuu + "." + sunniaasta)
 
 //Toob välja eraldi sünnikuupäeva//
